6d5800d1a6c64e65f9ce5d7a5431479ecf6f7b5a
Armstrong0605 <jennerarmstrong326@gmail.com>
2024-08-09 19:35:23 +0530

    Capastone 2

--- src/test/java/Utility/Listener.java
+++ src/test/java/Utility/Listener.java
@@ -1,69 +1,82 @@
 package Utility;
 
-import java.io.File;
-import java.io.IOException;
 
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+	import java.io.File;
+	import java.io.IOException;
+
+	import org.testng.ITestContext;
+	import org.testng.ITestListener;
+	import org.testng.ITestResult;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
+	import com.aventstack.extentreports.ExtentTest;
+	import com.aventstack.extentreports.Status;
 
 import Base.Projectspecification;
 
 
-public class Listener extends Projectspecification implements ITestListener{
-	
-	ExtentTest test;
-	ExtentReports extent = ExtentreportBestBuy.getReport();
 
-	@Override
-	public void onTestStart(ITestResult result) {
-		
-		
-		test = extent.createTest(result.getMethod().getMethodName());
-	}
 
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		
-		test.log(Status.PASS, "Test passed");
-	}
+	public class Listener extends Projectspecification implements ITestListener
 
-	@Override
-	public void onTestFailure(ITestResult result)
 	{
-		test.fail(result.getThrowable());
-		
-		String filepath = null;
-		try 
-		{
-			filepath = screenShot(result.getMethod().getMethodName());
-		} 
-		catch (IOException e) 
-		{
+		ExtentTest test;
+		ExtentReports extent = ExtentreportBestBuy.getReport();
+
+		@Override
+		public void onTestStart(ITestResult result) {
 			
-			e.printStackTrace();
+			test = extent.createTest(result.getMethod().getMethodName());
+		}
+
+/*		@Override
+		public void onStart(ITestContext context) {
+			
+			test = extent.createTest("Check Listener");
+		}*/
+
+		@Override
+		public void onTestSuccess(ITestResult result) {
+			
+			test.log(Status.PASS, "Test passed");
+		}
+
+		@Override
+		public void onTestFailure(ITestResult result)
+		{
+			test.fail(result.getThrowable());
+			
+			String filepath = null;
+			try 
+			{
+				filepath = screenShot(result.getMethod().getMethodName());
+			} 
+			catch (IOException e) 
+			{
+				
+				e.printStackTrace();
+			}
+			
+			test.addScreenCaptureFromPath(filepath,result.getMethod().getMethodName());
+		}  
+
+		@Override
+		public void onTestSkipped(ITestResult result) {
+			
+			System.out.println("Test skipped");
+		}
+
+
+		@Override
+		public void onFinish(ITestContext context) {
+			
+			extent.flush();
 		}
 		
-		test.addScreenCaptureFromPath(filepath,result.getMethod().getMethodName());
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult result) {
-		
-		System.out.println("Test skipped");
 	}
 
 
-	@Override
-	public void onFinish(ITestContext context) {
-		
-		extent.flush();
-	}
-	
-	
 
-}
+
